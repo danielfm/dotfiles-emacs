ed873400ce509f32e56ee12726c63cd4f61ac92b
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-01-20 19:31:43 -0200

    Fixed indentation.

--- packages.el
+++ packages.el
@@ -5,9 +5,9 @@
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-(let (el-get-master-branch)
-    (goto-char (point-max))
-    (eval-print-last-sexp))))
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
 ;; load all .el files inside `modules-dir`
 (setq modules-dir (expand-file-name "modules" emacs-root-dir))
