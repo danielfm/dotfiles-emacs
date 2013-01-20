f229ca11cc2ae4211d258a4359d3c6b7551fb9b2
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-01-20 19:29:01 -0200

    Removed unnecessary comment.

--- modules/irc.el
+++ modules/irc.el
@@ -1,5 +1,3 @@
-;; set the environment variables IRCNICK to your nickname
-;; http://www.gnu.org/software/emacs/manual/html_mono/erc.html
 (add-to-list 'el-get-sources
 	     '(:name erc
 		     :after (progn
