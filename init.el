1bcab21a21b31b8b15ebf4cad7a0d6fbda08cf2e
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2013-02-04 08:03:24 -0200

    Moved the file that loads the shell environment to a dedicated file that runs before the initialization of the package system.

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 (add-to-list 'load-path emacs-root-dir)
 
 (load "functions")
+(load "env")
 (load "packages")
 (load "editor")
 (load "ui")
--- window.el
+++ window.el
@@ -6,8 +6,3 @@
 (global-set-key (kbd "M-S-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "M-S-<down>")  'enlarge-window)
 (global-set-key (kbd "M-S-<left>")  'shrink-window-horizontally)
-
-;; loads the shell environment variables when launching Emacs outside shell
-(when (and window-system
-	   (in-list-p system-type '(darwin gnu/linux)))
-  (load-unix-shell-env))
