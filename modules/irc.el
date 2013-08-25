18eaaf4459d2785b8451783be3e8c1a1d6a6a6b3
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-08-25 09:35:08 -0300

    Added inline image support for ERC.

--- modules/irc.el
+++ modules/irc.el
@@ -1,2 +1,12 @@
 (add-to-list 'el-get-sources
 	     '(:name erc))
+
+(add-to-list 'el-get-sources
+	     '(:name erc-image
+		     :type github
+		     :pkgname "jondeandres/erc-image.el"
+		     :description "Fetch and show received images in a ERC buffer"
+		     :url "https://github.com/jondeandres/erc-image.el"
+		     :after (progn
+                              (require 'erc-image)
+                              (add-to-list 'erc-modules 'image))))
