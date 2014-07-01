1ebd4e2ed031c2e4c6b57d4a998b83b1e26ae7b5
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2014-07-01 14:17:45 -0300

    Also sets the `org-directory` var.

--- modules/org.el
+++ modules/org.el
@@ -1,4 +1,5 @@
 ;; Keeps the agenda in an index file on disk
 (let (agenda-dir (getenv "ORG_AGENDA_INDEX_FILE"))
   (when agenda-dir
+    (setq org-directory (file-name-directory agenda-dir))
     (setq org-agenda-files agenda-dir)))
