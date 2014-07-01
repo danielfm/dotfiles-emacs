4bf5c5822bed717795744d841cd33213b20e9b1f
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2014-07-01 18:25:03 -0300

    Initial (very basic) Org-mode configuration.

--- modules/org.el
+++ modules/org.el
@@ -3,3 +3,10 @@
   (when agenda-dir
     (setq org-directory (file-name-directory agenda-dir))
     (setq org-agenda-files agenda-dir)))
+
+;; Asks for a note when a TODO task is completed
+(setq org-log-done 'note)
+
+;; Custom TODO workflow
+(setq org-todo-keywords
+      '((sequence "TODO" "WIP" "|" "DONE" "DELEGATED")))
