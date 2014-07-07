0681c3a47189331090dd0ced6d51a71457293afc
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2014-07-07 14:08:05 -0300

    Added `discover-my-major` mode.

--- modules/editor.el
+++ modules/editor.el
@@ -5,3 +5,13 @@
                               (global-set-key (kbd "C-c >") 'mc/mark-next-like-this)
                               (global-set-key (kbd "C-c <") 'mc/mark-previous-like-this)
                               (global-set-key (kbd "C-c .") 'mc/mark-all-like-this))))
+
+(add-to-list 'el-get-sources
+             '(:name discover-my-major
+                     :type git
+                     :url "https://github.com/steckerhalter/discover-my-major"
+                     :after (progn
+                              (global-set-key (kbd "C-h C-m") 'discover-my-major))))
+
+(add-to-list 'el-get-sources
+             '(:name makey))
