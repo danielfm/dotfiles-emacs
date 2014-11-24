d661febf19591e767e15bb2466210ca5cd1c7adf
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2014-11-24 17:07:37 -0200

    Sets `org-return-follows-link` var.

--- modules/org.el
+++ modules/org.el
@@ -1,12 +1,15 @@
-;; Keeps the agenda in an index file on disk
+;; keeps the agenda in an index file on disk
 (let (agenda-dir (getenv "ORG_AGENDA_INDEX_FILE"))
   (when agenda-dir
     (setq org-directory (file-name-directory agenda-dir))
     (setq org-agenda-files agenda-dir)))
 
-;; Asks for a note when a TODO task is completed
+;; asks for a note when a TODO task is completed
 (setq org-log-done 'note)
 
-;; Custom TODO workflow
+;; opens link under cursor in ORG mode when <RET> is pressed
+(setq org-return-follows-link 1)
+
+;; custom TODO workflow
 (setq org-todo-keywords
       '((sequence "TODO" "WIP" "|" "DONE" "DELEGATED" "PRODUCTION")))
