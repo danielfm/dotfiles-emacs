cb814afa8deeb1f8c6539691d23cdcc8cc7cc707
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2015-03-09 14:23:36 -0300

    Adjusted a few settings of web-mode.

--- modules/markup.el
+++ modules/markup.el
@@ -8,4 +8,7 @@
 	     '(:name web-mode
 		     :after (progn
                               (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-                              (add-hook 'sgml-mode-hook 'web-mode))))
+                              (add-hook 'sgml-mode-hook 'web-mode)
+                              (setq web-mode-markup-indent-offset 2)
+                              (setq web-mode-css-indent-offset 2)
+                              (setq web-mode-code-indent-offset 2))))
