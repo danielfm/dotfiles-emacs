086b179afde16325180298db607fb475acf41061
Daniel Fernandes Martins <daniel.martins@descomplica.com.br>
2015-07-24 11:33:13 -0300

    Run `go fmt' before saving a Go source file.

--- modules/go.el
+++ modules/go.el
@@ -1,2 +1,7 @@
 (add-to-list 'el-get-sources
-	     '(:name go-mode))
+	     '(:name go-mode
+                     :after (progn
+                              (add-hook 'before-save-hook #'gofmt-before-save))))
+
+(add-to-list 'el-get-sources
+             '(:name go-lint))
