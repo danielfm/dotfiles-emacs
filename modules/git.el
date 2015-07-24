afc7aaa22421320f9d0635ffbb6108b8a516f81d
Daniel Fernandes Martins <daniel.martins@descomplica.com.br>
2015-07-24 11:32:25 -0300

    Magit upgrade.

--- modules/git.el
+++ modules/git.el
@@ -2,4 +2,5 @@
 	     '(:name magit
 		     :after (progn
                               (global-set-key (kbd "C-x g") 'magit-status)
-                              (setq magit-auto-revert-mode nil))))
+                              (setq magit-auto-revert-mode nil)
+                              (setq magit-last-seen-setup-instructions "1.4.0"))))
