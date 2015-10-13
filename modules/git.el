92d30628fe082be3452a010bee5cfc2692fced21
Daniel Martins <daniel.martins@descomplica.com.br>
2015-10-13 09:41:41 -0300

    Updated magit setup.

--- modules/git.el
+++ modules/git.el
@@ -3,4 +3,5 @@
 		     :after (progn
                               (global-set-key (kbd "C-x g") 'magit-status)
                               (setq magit-auto-revert-mode nil)
+                              (setq magit-push-always-verify nil)
                               (setq magit-last-seen-setup-instructions "1.4.0"))))
