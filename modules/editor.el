4a511361fe1ec000b7ef49f7143acf285ab7181b
Daniel Martins <daniel.martins@descomplica.com.br>
2015-10-26 12:38:04 -0200

    Added more multiple-cursor shortcuts.

--- modules/editor.el
+++ modules/editor.el
@@ -4,7 +4,9 @@
                               (global-set-key (kbd "C-c *") 'mc/edit-lines)
                               (global-set-key (kbd "C-c >") 'mc/mark-next-like-this)
                               (global-set-key (kbd "C-c <") 'mc/mark-previous-like-this)
-                              (global-set-key (kbd "C-c .") 'mc/mark-all-like-this))))
+                              (global-set-key (kbd "C-c .") 'mc/mark-all-like-this)
+                              (global-set-key (kbd "C-c i") 'mc/mark-more-like-this-extended))))
+
 
 (add-to-list 'el-get-sources
              '(:name editorconfig
