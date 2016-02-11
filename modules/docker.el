681f665ec27e9218230148d93fe1c00f430ba541
Daniel Martins <daniel.martins@descomplica.com.br>
2016-02-11 11:17:42 -0200

    Handles Docker's `.env' file properly.

--- modules/docker.el
+++ modules/docker.el
@@ -1,2 +1,4 @@
 (add-to-list 'el-get-sources
-	     '(:name dockerfile-mode))
+	     '(:name dockerfile-mode
+                     :after (progn
+                              (add-to-list 'auto-mode-alist (cons "\\.env" 'conf-mode)))))
