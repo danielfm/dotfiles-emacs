6f0a91a72d8d43022b66cb2307f03c7a65d1528d
Daniel Martins <daniel.martins@descomplica.com.br>
2016-02-29 09:46:17 -0300

    Removed mode.

--- modules/editor.el
+++ modules/editor.el
@@ -32,11 +32,6 @@
 (add-to-list 'el-get-sources
              '(:name makey))
 
-(add-to-list 'el-get-sources
-             '(:name powerline
-                     :after (progn
-                              (powerline-default-theme))))
-
 ;; better clipboard integration in osx and terminal emacs
 (add-to-list 'el-get-sources
              '(:name pbcopy
