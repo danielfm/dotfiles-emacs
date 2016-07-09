e211317d9fc7cef1270973f3bea5725f1bc05f46
Daniel Martins <daniel.martins@descomplica.com.br>
2016-07-09 12:30:04 -0300

    Change JS mode for better ECMAScript support.

--- modules/javascript.el
+++ modules/javascript.el
@@ -1,3 +1,6 @@
 (add-to-list 'el-get-sources
-	     '(:name js3-mode
-		     :after (setq js3-highlight-external-variables nil)))
+	     '(:name js2-mode))
+
+(add-to-list 'el-get-sources
+             '(:name ecmascript-mode
+                     :after (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))))
