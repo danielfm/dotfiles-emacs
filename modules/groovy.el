bd2d5938aaa21bed0bc90f29f5924ea1099eefbf
Daniel Fernandes Martins <daniel.martins@descomplica.com.br>
2017-02-14 15:18:23 -0200

    Use groovy-mode for Jenkinsfile

--- modules/groovy.el
+++ modules/groovy.el
@@ -1,2 +1,7 @@
+(setq groovy-files '("Jenkinsfile"))
+
 (add-to-list 'el-get-sources
-	     '(:name groovy-emacs-mode))
+	     '(:name groovy-emacs-mode
+                     :after (mapc (lambda (file)
+				    (add-to-list 'auto-mode-alist (cons file 'groovy-mode)))
+				  groovy-files)))
