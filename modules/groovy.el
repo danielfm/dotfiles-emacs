b10177596c87eeb4d28d05e4e4ece8a24175b72e
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2017-08-01 14:06:39 -0300

    Removed unused configuration.

--- modules/groovy.el
+++
@@ -1,7 +0,0 @@
-(setq groovy-files '("Jenkinsfile"))
-
-(add-to-list 'el-get-sources
-	     '(:name groovy-emacs-mode
-                     :after (mapc (lambda (file)
-				    (add-to-list 'auto-mode-alist (cons file 'groovy-mode)))
-				  groovy-files)))
--- modules/http.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'el-get-sources
-             '(:name restclient
-                     :type github
-                     :description "HTTP REST client tool for emacs"
-                     :pkgname "pashky/restclient.el"
-                     :after (progn
-                              (require 'restclient)
-                              (add-to-list 'auto-mode-alist '("\\.http" . restclient-mode)))))
