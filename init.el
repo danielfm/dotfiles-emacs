66d9e3629737a7c8ec3ae6bb5fb99c170683a78e
Daniel Martins <daniel.tritone@gmail.com>
2017-11-10 16:11:13 -0200

    Move functions to the (only) file where they are used.

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
 (setq default-directory "~/.emacs.d/")
 (add-to-list 'load-path (expand-file-name "./lisp/" default-directory))
 
-(load "functions")
 (load "env")
 (load "packages")
 (load "editor")
