5d3f82ab41474ee7cb8a47e68a1bd5ebb7bcb5c6
Daniel Martins <daniel.martins@descomplica.com.br>
2019-01-14 10:42:22 -0200

    Auto-enable paredit mode

--- lisp/packages.el
+++ lisp/packages.el
@@ -59,7 +59,14 @@
 ;; editing experience
 
 (use-package paredit
-  :ensure t)
+  :ensure t
+  :config (progn
+            (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+            (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+            (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+            (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+            (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+            (add-hook 'scheme-mode-hook           #'enable-paredit-mode)))
 
 (use-package highlight-parentheses
   :ensure t)
