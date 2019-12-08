7d2ed2a2500ccdf569cf36e4d29d44229ca46992
Daniel Martins <daniel.tritone@gmail.com>
2019-12-08 09:17:31 -0300

    Launch script now handles term-only environments

--- launch.sh
+++ launch.sh
@@ -3,6 +3,15 @@
 # First tries to launch a new client frame. If the server is not up, then
 # launches a regular emacs instance.
 
-if ! emacsclient -c "$@"; then
-  emacs "$@"
+# If launching without a grafical environment, uses 'emacsclient -nw'
+# instead of 'emacsclient -c'.
+
+if [ -z "$DISPLAY" ]; then
+  if ! emacsclient -nw "$@"; then
+    emacs "$@"
+  fi
+else
+  if ! emacsclient -c "$@"; then
+    emacs "$@"
+  fi
 fi
