aa60770c1e34284a730d301b67a12a668d91b8a0
Daniel Martins <daniel.tritone@gmail.com>
2020-02-17 12:57:22 -0300

    Remove spotify config

--- lisp/packages.el
+++ lisp/packages.el
@@ -11,7 +11,7 @@
 
 ;; package configuration
 
-;; themes
+;; theme
 
 (use-package material-theme
   :ensure t
@@ -127,7 +127,8 @@
                                      ,(concat org-directory "/tickler.org_archive")
                                      ,(concat org-directory "/gtd.org")
                                      ,(concat org-directory "/gtd.org_archive")
-                                     ,(concat org-directory "/inbox.org")))
+                                     ,(concat org-directory "/inbox.org")
+                                     ,(concat org-directory "/someday.org")))
             ;; capture templates
             (setq org-capture-templates `(("t" "Todo [inbox]" entry
                                            (file+headline ,(concat org-directory "/inbox.org") "Inbox")
@@ -136,9 +137,9 @@
                                            (file+headline ,(concat org-directory "/tickler.org") "Tickler")
                                            "* %i%? \n %U")))
 
-            (setq org-refile-targets `((,(concat org-directory "/gtd.org") :maxlevel . 3)
-                                       (,(concat org-directory "/someday.org") :level . 1)
-                                       (,(concat org-directory "/tickler.org") :maxlevel . 2)))
+            (setq org-refile-use-outline-path t)
+            (setq org-refile-targets '((nil :maxlevel . 3)
+                                       (org-agenda-files :maxlevel . 9)))
 
             ;; workflow steps
             (setq org-todo-keywords '((sequence "TODO(t)" "WIP(w)" "REVIEW(r)" "|" "DONE(d@)" "CANCELED(c@)")))
@@ -159,40 +160,3 @@
             (diminish 'paredit-mode " ()")
             (diminish 'flyspell-mode " ~")
             (diminish 'eldoc-mode " doc")))
-
-;; spotify
-
-(setq spotify-keymap-prefix "M-p")
-
-(add-to-list 'load-path "~/Projects/spotify.el")
-(require 'spotify)
-
-(setq spotify-oauth2-client-id "543b87d8c96e4a908ef0188d1fb8f8c1")
-(setq spotify-oauth2-client-secret "8c2b9a00f25849e383598e62fed01e8d")
-
-(setq spotify-genius-token "Dxpe0urpYxZ0NQhKjGrkJJTXh9HXGkxgzTUd_xKfrakBplDq5PkO2sociIuV6piv")
-
-(setq spotify-mode-line-refresh-interval 5)
-(setq spotify-player-status-refresh-interval 5)
-
-(setq spotify-mode-line-truncate-length 20)
-(setq spotify-player-status-truncate-length 20)
-
-(setq spotify-mode-line-playing-text "P")
-(setq spotify-player-status-playing-text "P")
-(setq spotify-mode-line-paused-text "-")
-(setq spotify-player-status-paused-text "-")
-(setq spotify-mode-line-stopped-text "-")
-(setq spotify-player-status-stopped-text "-")
-
-(setq spotify-mode-line-format "[%a - %t][%p%r%s]")
-(setq spotify-player-status-format "[%a - %t][%p%r%s]")
-
-(setq spotify-api-locale "pt_BR")
-(setq spotify-api-country "BR")
-
-(setq spotify-transport 'connect)
-
-;; (global-spotify-remote-mode t)
-
-
